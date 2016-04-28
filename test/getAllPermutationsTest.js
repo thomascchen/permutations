84310717fcfaef4e6de6ece6688813aee8ec95a5
Brian Fink <Brian.Fink@CVSCaremark.com>
2016-04-27 21:47:29 -0400

    Technically works but not really

--- test/getAllPermutationsTest.js
+++ test/getAllPermutationsTest.js
@@ -1,14 +1,16 @@
-// var chill = require('../main.js');
+var assert = require('assert');
+var chill = require('../main.js');
 
-// describe('Function', function() {
-// 	describe('getAllPermutations(Array)', function() {
-//
-// 		it('Returns all permutations of an array', function(done) {
-// 			var array = arrayGenerator(6);
-// 			getAllPermutations(array, true);
-//
-//       done();
-// 		});
-//
-// 	});
-// });
+describe('Function', function() {
+	describe('getAllPermutations(Array)', function() {
+
+		it('Returns all permutations of an array', function(done) {
+			var array = chill.arrayGenerator(6);
+			var permutations = chill.getAllPermutations(array, true);
+
+      assert.equal(permutations.length, chill.factorial(array.length));
+      done();
+		});
+
+	});
+});
--- test/getPermutationsTest.js
+++ test/getPermutationsTest.js
@@ -1,27 +1,29 @@
-// var chill = require('../main.js');
+var assert = require('assert');
+var chill = require('../main.js');
 
-// require('../lib/requireAll.js');
-//
-// describe('Function', function() {
-// 	describe('getPermutations(Array)', function() {
-//
-// 		it('Returns many permutations of an array', function(done) {
-// 			var array = arrayGenerator(9);
-//       var permutations = getPermutations(array);
-//       var boolean = permutations.every(function(permutation) {
-//         assert.deepEqual(permutation.sort(), permutation);
-//       });
-//
-//       done();
-// 		});
-//
-// 		it('Doesn\'t alter the original array', function(done) {
-// 			var array = arrayGenerator(9);
-// 			var permutations = getPermutations(array);
-//       assert.deepEqual(array, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
-//
-//       done();
-// 		});
-//
-// 	});
-// });
+describe('Function', function() {
+	describe('chill.getPermutations(Array)', function() {
+
+		it('Returns many permutations of an array', function(done) {
+			var array = chill.arrayGenerator(9);
+      var permutations = chill.getPermutations(array);
+
+      var boolean = permutations.every(function(permutation) {
+        return chill.equalArrays(permutation.sort(), array);
+      });
+
+			assert.ok(permutations.length > 0)
+			assert.equal(boolean, true);
+      done();
+		});
+
+		it('Doesn\'t alter the original array', function(done) {
+			var array = chill.arrayGenerator(9);
+			var permutations = chill.getPermutations(array);
+      assert.deepEqual(array, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+      done();
+		});
+
+	});
+});
