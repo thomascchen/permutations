c75612a856ef391c3df01a13879d204c8965828c
Brian Fink <Brian.Fink@CVSCaremark.com>
2016-04-30 09:53:59 -0400

    Update comments

--- main.js
+++ main.js
@@ -1,9 +1,63 @@
+// Great a sequential array of specified length
+
 function arrayGenerator(length) {
   var array = [];
   for (var i = 1; i <= length; i++) {
     array.push(i);
   }
   return array;
+};
+
+// Crazy bogo way to get permutations of an array
+
+function bogoAllPermutations(array, debugMode) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  if (array.length === 2) {
+    return [array, array.reverse()];
+  }
+
+  var possibilities = factorial(array.length);
+  var permutations = bogoSomePermutations(array);
+
+  while (uniq(permutations).length < possibilities) {
+    if (debugMode) {
+      console.log(permutations.length);
+    }
+    array = shuffle(array);
+    flatPush(permutations, bogoSomePermutations(array));
+  }
+
+  if (debugMode) {
+    console.log(permutations.length);
+    console.log(possibilities);
+    console.log('Success!');
+  }
+
+  return uniq(permutations);
+}
+
+// Bogo getting of some permuations
+
+function bogoSomePermutations(array) {
+  var length = array.length;
+  var permutations = [];
+  var newSet;
+  var newArray = copyArray(array);
+
+  for (var i = 0; i < length; i++) {
+    newSet = doubleCycleAll(newArray);
+    flatPush(permutations, newSet);
+    newArray = rotate(newArray, 1);
+  }
+
+  permutations = permutations.filter(function(thisArray) {
+    return thisArray.length === length;
+  });
+
+  return uniq(permutations);
 };
 
 // Make n copies of array
@@ -27,6 +81,9 @@ function copyArray(array, n) {
   return copies;
 };
 
+// Shift every array element to every position, while maintaining the other
+// elements in place
+
 function cycleAll(array) {
   var n = array.length;
   var permutations = [];
@@ -42,6 +99,7 @@ function cycleAll(array) {
 };
 
 // Shift array element at index x to every possible position
+
 function cycleElement(array, x) {
   var permutations1 = cycleToEnd(array, x);
   var permutations2 = cycleToFront(array, x);
@@ -51,6 +109,7 @@ function cycleElement(array, x) {
 }
 
 // Shift array element at index x to every possible position from current to end
+
 function cycleToEnd(array, indexToCycle) {
   var i = indexToCycle
   var length = array.length;
@@ -66,6 +125,7 @@ function cycleToEnd(array, indexToCycle) {
 };
 
 // Shift array element at index x to every possible position toward front
+
 function cycleToFront(array, indexToCycle) {
   var i = indexToCycle
   var permutations = [array];
@@ -79,11 +139,15 @@ function cycleToFront(array, indexToCycle) {
   return permutations;
 };
 
+// Delete all instances of a given value
+
 function deleteAll(array, toDelete) {
   return array.filter(function(element) {
     return element !== toDelete
   });
 };
+
+// Delete leftmost occurence of given element
 
 function deleteLeftmost(array, toDelete) {
   var newArray = [];
@@ -100,11 +164,15 @@ function deleteLeftmost(array, toDelete) {
   return (newArray);
 };
 
+// Delete rightmost occurence of given element
+
 function deleteRightmost(array, toDelete) {
   var array = reverse(array);
   array = deleteLeftmost(array, toDelete);
   return reverse(array);
 };
+
+// Cycle all elements, reverse, cycle again
 
 function doubleCycleAll(array) {
   var permutations1 = cycleAll(array);
@@ -112,6 +180,8 @@ function doubleCycleAll(array) {
 
   return uniq(permutations1.concat(permutations2));
 };
+
+// Determine if two arrays are equal
 
 function equalArrays(array1, array2) {
   if (array1.length !== array2.length) {
@@ -121,27 +191,29 @@ function equalArrays(array1, array2) {
     return equalArrays2(array1, array2);
   }
   return equalArrays1(array1, array2);
+
+  function equalArrays1(array1, array2) {
+    var equal = true;
+    array1.forEach(function(element, index) {
+      if (array1[index] !== array2[index]) {
+        equal = false;
+      }
+    });
+    return equal;
+  }
+
+  function equalArrays2(metaArray1, metaArray2) {
+    var equal = true;
+    metaArray1.forEach(function(subArray, index) {
+      if (!equalArrays1(metaArray1[index], metaArray2[index])) {
+        equal = false;
+      }
+    });
+    return equal;
+  }
 };
 
-function equalArrays1(array1, array2) {
-  var equal = true;
-  array1.forEach(function(element, index) {
-    if (array1[index] !== array2[index]) {
-      equal = false;
-    }
-  });
-  return equal;
-}
-
-function equalArrays2(metaArray1, metaArray2) {
-  var equal = true;
-  metaArray1.forEach(function(subArray, index) {
-    if (!equalArrays1(metaArray1[index], metaArray2[index])) {
-      equal = false;
-    }
-  });
-  return equal;
-}
+// Return factorial of n
 
 function factorial(positiveNumber) {
   var n = positiveNumber;
@@ -151,87 +223,71 @@ function factorial(positiveNumber) {
   return n
 };
 
+// Combine all the elments of two arrays
+
 function flatPush(targetArray, sourceArray) {
   sourceArray.forEach(function(element) {
     targetArray.push(element);
   });
 };
 
-function getAllPermutations(array, debugMode) {
-  if (array.length <= 1) {
-    return array;
-  }
-
-  if (array.length === 2) {
-    return [array, array.reverse()];
-  }
-
-  var possibilities = factorial(array.length);
-  var permutations = getPermutations(array);
-
-  while (uniq(permutations).length < possibilities) {
-    if (debugMode) {
-      console.log(permutations.length);
-    }
-    array = shuffle(array);
-    flatPush(permutations, getPermutations(array));
-  }
-
-  if (debugMode) {
-    console.log(permutations.length);
-    console.log(possibilities);
-    console.log('Success!');
-  }
-
-  return uniq(permutations);
-}
-
-function getPermutations(array) {
-  var length = array.length;
-  var permutations = [];
-  var newSet;
-  var newArray = copyArray(array);
-
-  for (var i = 0; i < length; i++) {
-    newSet = doubleCycleAll(newArray);
-    flatPush(permutations, newSet);
-    newArray = rotate(newArray, 1);
-  }
-
-  permutations = permutations.filter(function(thisArray) {
-    return thisArray.length === length;
-  });
-
-  return uniq(permutations);
-};
+// Does array include element true / false
 
 function includes(array, element) {
   if (typeof array[0] === 'object') {
     return includes2(array, element);
   }
   return includes1(array, element);
+
+  function includes1(array, element) {
+    if (array.indexOf(element) > -1) {
+      return true;
+    }
+    return false;
+  }
+
+  function includes2(metaArray, array) {
+    var includes = false;
+    metaArray.forEach(function(element) {
+      if (equalArrays(array, element)) {
+        includes = true;
+      }
+    });
+    return includes;
+  }
 };
 
-function includes1(array, element) {
-  if (array.indexOf(element) > -1) {
-    return true;
-  }
-  return false;
-}
-
-function includes2(metaArray, array) {
-  var includes = false;
-  metaArray.forEach(function(element) {
-    if (equalArrays(array, element)) {
-      includes = true;
-    }
-  });
-  return includes;
-}
+// Returns last element of an array
 
 function lastElement(array) {
   return array[array.length - 1];
 };
+
+// Get all permutations of an array
+
+function permutations(input) {
+  var permArr = [],
+    usedChars = [];
+
+  function permute(input) {
+    var i, ch;
+    for (i = 0; i < input.length; i++) {
+      ch = input.splice(i, 1)[0];
+      usedChars.push(ch);
+      if (input.length == 0) {
+        permArr.push(usedChars.slice());
+      }
+      permute(input);
+      input.splice(i, 0, ch);
+      usedChars.pop();
+    }
+    return permArr
+  };
+
+  return permute(input);
+}
+
+// Returns a reversed array
 
 function reverse(array) {
   var newArray = [];
@@ -240,6 +296,8 @@ function reverse(array) {
   });
   return newArray;
 }
+
+// Rotates an array n times
 
 function rotate(array, n) {
   if (!n) {
@@ -255,6 +313,8 @@ function rotate(array, n) {
   return array;
 }
 
+// Randomly shuffles an array
+
 function shuffle(array) {
   for (var i = 0; i < array.length; i++) {
     var x = getRandom(0, array.length);
@@ -268,11 +328,15 @@ function getRandom(min, max) {
   return Math.floor((Math.random() * max) + min);
 }
 
+// Converts all elements of an array into strings
+
 function stringifyElements(array) {
   return array.map(function(element) {
     return element.toString();
   });
 };
+
+// Switchs element at inde1 with element at index2
 
 function swapElements(array, index1, index2) {
   var newArray = copyArray(array, 1);
@@ -281,37 +345,41 @@ function swapElements(array, index1, index2) {
   return newArray;
 }
 
+// Returns a uniq copy of an array
+
 function uniq(array, element) {
   if (typeof array[0] === 'object') {
     return uniq2(array);
   }
   return uniq1(array);
-};
 
-function uniq1(array) {
-  var uniqArray = [];
-  array.forEach(function(element, index) {
-    if (includes(uniqArray, element)) {
-      return;
-    }
-    uniqArray.push(array[index]);
-  })
-  return uniqArray;
-};
+  function uniq1(array) {
+    var uniqArray = [];
+    array.forEach(function(element, index) {
+      if (includes(uniqArray, element)) {
+        return;
+      }
+      uniqArray.push(array[index]);
+    })
+    return uniqArray;
+  };
 
-function uniq2(array) {
-  var uniqArray = [];
-  array.forEach(function(element, index) {
-    if (includes(uniqArray, element)) {
-      return;
-    }
-    uniqArray.push(array[index]);
-  })
-  return uniqArray;
+  function uniq2(array) {
+    var uniqArray = [];
+    array.forEach(function(element, index) {
+      if (includes(uniqArray, element)) {
+        return;
+      }
+      uniqArray.push(array[index]);
+    })
+    return uniqArray;
+  };
 };
 
 module.exports = {
   arrayGenerator: arrayGenerator,
+  bogoAllPermutations: bogoAllPermutations,
+  bogoSomePermutations: bogoSomePermutations,
   copyArray: copyArray,
   cycleAll: cycleAll,
   cycleElement: cycleElement,
@@ -324,10 +392,9 @@ module.exports = {
   equalArrays: equalArrays,
   factorial: factorial,
   flatPush: flatPush,
-  getAllPermutations: getAllPermutations,
-  getPermutations: getPermutations,
   includes: includes,
   lastElement: lastElement,
+  permutations: permutations,
   reverse: reverse,
   rotate: rotate,
   shuffle: shuffle,
